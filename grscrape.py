c1983eb74de1b83d22fa161cd31cb0dee1a9cb31
MANISH <manishsrinivasan2003@gmail.com>
2023-02-05 17:44:57 +0530

    goodreads scraper
    this script scraps goodreads for book information liketitle author description and also downloads cover image

---
+++ grscrape.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+foldername='bookimgs'
+if not (os.path.exists(f'./{foldername}')):
+    os.mkdir(f'{foldername}')
+
+def geturl(bname):
+     searchurl=f"https://www.goodreads.com/search?q={bname}"
+     searchpage= requests.get(searchurl).content
+     soup = BeautifulSoup(searchpage, 'lxml')
+     tag=soup.find('a',class_='bookTitle')
+     return "https://www.goodreads.com"+tag.get('href')
+
+def getpost(bookurl):
+    bookpage= requests.get(bookurl).content
+    soup= BeautifulSoup(bookpage, 'lxml')
+    title= soup.find('h1', attrs={'data-testid': 'bookTitle'}).text
+    authors= ", ".join([i.text for i in soup.find('div', class_='BookPageMetadataSection__contributor').find_all('span',class_='ContributorLink__name')])
+    imgsrc = soup.find('img',class_='ResponsiveImage').get('src')
+    image= requests.get(imgsrc).content
+    with open(f'{foldername}/{title}.png','wb+') as f:
+        f.write(image)
+    rating = float(soup.find('div', class_="RatingStatistics__rating").text)
+    star='⭐'
+    stars = star*round(rating)
+    desc = soup.find('div',class_='BookPageMetadataSection__description').find('span',class_='Formatted').get_text()
+    post = \
+    f"""
+    {title}
+    {authors}
+    {stars} {rating}
+
+    {desc}
+    """
+    return post
+
+
+book = input("enter a book name to search: ")
+bookurl = geturl(book)
+print(getpost(bookurl))
